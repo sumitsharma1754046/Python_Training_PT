2e65397ea8791c99cfa777714135f07792bec5ee
python_training <sumitsharma1754046@gmail.com>
2024-11-21 17:09:37 +0530

    Vehicle Class Defined

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,8 @@
 print("Hello world")
 print("this is the Development Environment")
+class Vehicle:
+    def configuration(self):
+        print("This is Car")
+
+Tata = Vehicle()
+Tata.configuration()
