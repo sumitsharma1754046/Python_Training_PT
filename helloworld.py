3e3cf860f266dd6e3b87f330bb1a7092de2e576d
python_training <sumitsharma1754046@gmail.com>
2024-11-22 10:54:08 +0530

    new function

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,6 @@ print("this is the Development Environment")
 class Vehicle:
     def configuration(self):
         print("This is Car")
-
+print("Function is calling below")
 Tata = Vehicle()
 Tata.configuration()
