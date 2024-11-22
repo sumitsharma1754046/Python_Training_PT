aafb75b07741d366556a153213c46822a5293002
python_training <sumitsharma1754046@gmail.com>
2024-11-22 10:56:30 +0530

    Commit this

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,8 @@ print("this is the Development Environment")
 class Vehicle:
     def configuration(self):
         print("This is Car")
+
 print("Function is calling below")
 Tata = Vehicle()
 Tata.configuration()
+print("Function is called above")
